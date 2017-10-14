547f4b22d9b9c994903620479dbcd6cc1d5d5d1a
John Piesco <jpaul@iamxml.com>
2017-10-14 01:13:58 -0400

    Update LocationController.cs
    Fixed bug in line 25

--- LocationController.cs
+++ LocationController.cs
@@ -5,24 +5,26 @@ using Newtonsoft.Json.Linq;
 using System.IO;
 using System.Text;
 
-namespace MyDotNetCoreProject.Controllers
+namespace MyWebAPI.Controllers
 {
     [Produces("application/json")]
     [Route("api/Location")]
     public class LocationController : Controller
     {
         [EnableCors("SiteCorsPolicy")]//Remove this if CORS is not configured, not needed unless using CrossOriginResourceSharing
-        [HttpGet]
-        public object Get(locationClass latlng)
+        [HttpPost]
+        public string Post(string lat, string lng)
         {
-            string lat = latlng.lat;
-            string lng = latlng.lng;
-            var file = System.IO.Path.GetFullPath("../Data/Location/location.txt");
+
+            var file = System.IO.Path.GetFullPath("../Data/Location/Location.txt");
             string allText = System.IO.File.ReadAllText(file);
+            //allText = allText.Replace("\\", "");
 
-            var jsonObj = JObject.Parse(allText);
+            JObject jsonObj = JObject.Parse(allText);
 
-            JObject locations = jsonObj["location"] as JObject;
+            string myJsonObj = jsonObj["location"].ToString();
+
+            JObject locations = JObject.Parse(myJsonObj);
 
             StringBuilder sb = new StringBuilder();
             StringWriter sw = new StringWriter(sb);
@@ -35,9 +37,9 @@ namespace MyDotNetCoreProject.Controllers
                 writer.WriteStartObject();
                 writer.WritePropertyName("location");
                 writer.WriteStartObject();
-                foreach (var record in locations)
+                foreach (var location in locations)
                 {
-                    var jsonObj1 = JObject.Parse(record.Value.ToString());
+                    var jsonObj1 = JObject.Parse(location.Value.ToString());
 
                     var checkLat = jsonObj1["lat"].ToString();
                     var checkLng = jsonObj1["lng"].ToString();
@@ -63,17 +65,11 @@ namespace MyDotNetCoreProject.Controllers
                 writer.WriteValue(lng);
                 writer.WriteEnd();
                 writer.WriteEndObject();
-                
+
             }
-            System.IO.File.WriteAllText(file, sw.ToString());
+             System.IO.File.WriteAllText(ile, sw.ToString());
 
-            return locations;
-        }
-        public class locationClass
-        {
-            public string lat { get; set; }
-
-            public string lng { get; set; }
+            return allText;
         }
     }
 }
